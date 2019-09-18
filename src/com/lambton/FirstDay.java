1f2120d99d0c8f631e466877ae18542911c5d8b9
macstudent <kamaljit1504>
2019-09-18 19:24:02 -0400

    one comment deleted

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -3,11 +3,14 @@ package com.lambton;
 public class FirstDay
 {
     public static void main(String args[]) {
-        System.out.println("Kamal");
+        // variable declared
         int a, b, sum;
+        //variable assignment
         a=10;
         b=20;
+        //perform add
         sum =a+b;
+        
         System.out.println("sum ="+sum);
 
     }
