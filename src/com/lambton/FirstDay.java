806c571b8574674c2b38ec29f64b5f13530dcb70
macstudent <kamaljit1504>
2019-09-18 19:27:00 -0400

    one file 2 fields

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -1,17 +1,13 @@
 package com.lambton;
 
-public class FirstDay
+public class StudentFile
 {
-    public static void main(String args[]) {
+
         // variable declared
-        int a, b, sum;
-        //variable assignment
-        a=10;
-        b=20;
-        //perform add
-        sum =a+b;
-        
-        System.out.println("sum ="+sum);
+        int Id=772733;
+        String name[]='Kamal';
+
+
 
     }
 }
