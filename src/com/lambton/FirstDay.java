b153191f000e4b39a78aee00511e190260ae289c
MacStudent <macstudent@126-iMac05.local>
2019-09-18 19:15:15 -0400

    Initial commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String args[]) {
+        System.out.println("Kamal");
+        int a, b, sum;
+        a=10;
+        b=20;
+        sum =a+b;
+        System.out.println("sum ="+sum);
+
+    }
+}
